f2d37d7de66b1958ce08e6b1a5b526949d03db52
tayef mahmud <mahmudovi183@gmail.com>
2024-10-23 16:34:53 +0600

    dashboard navbar z index fix

--- src/components/navbars/dashboard-navbar.jsx
+++ src/components/navbars/dashboard-navbar.jsx
@@ -1,3 +1,5 @@
 export function DashboardNavbar() {
-  return <nav className="bg-red-200 h-14 p-2">DashboardNavbar</nav>;
+  return (
+    <nav className="bg-red-50 sticky top-0 h-14 p-2 z-10">DashboardNavbar</nav>
+  );
 }
