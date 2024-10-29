95c4eed15e5f353189f586b06fe9cb676d9d7043
tayef mahmud <mahmudovi183@gmail.com>
2024-10-29 12:14:08 +0600

    sales product summary form

--- src/components/sales/sales-summary.jsx
+++ src/components/sales/sales-summary.jsx
@@ -1,26 +1,41 @@
 "use client";
-
 import { useSales } from "@/hooks/use-sales";
 import { SalesSummaryCard } from "./sales-summary-card";
 import { Heading } from "../heading";
-import { Button } from "../ui/button";
 import { useState } from "react";
 import { postData } from "@/utils/api-calls";
 import { useRouter } from "next/navigation";
 import { errorNotification, successNotification } from "@/utils/toast";
+import { FormModal } from "../form/form-modal";
+import { z } from "zod";
+import { FormInput } from "../form/form-input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const formSchema = z.object({
+  paid: z.string().optional().nullable(),
+});
 
 export function SalesSummary() {
   const [isLoading, setIsLoading] = useState(false);
   const { salesItems, total, onClear } = useSales();
   const router = useRouter();
 
-  const handleSubmit = async (e) => {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      paid: "",
+    },
+  });
+
+  const handleSubmit = async (data) => {
     setIsLoading(true);
 
     try {
       const res = await postData("sales", {
         amount: total,
         products: salesItems,
+        paid: data.paid,
       });
 
       if (res.error) {
@@ -45,10 +60,18 @@ export function SalesSummary() {
           <SalesSummaryCard key={index} product={product} />
         ))}
       </div>
-      <Heading>Total: ৳ {total / 100}</Heading>
-      <Button loading={isLoading} disabled={isLoading} onClick={handleSubmit}>
-        Save
-      </Button>
+      <FormModal
+        form={form}
+        formLabel="save"
+        onSubmit={handleSubmit}
+        loading={isLoading}
+        disabled={isLoading}
+      >
+        <Heading>Total: ৳ {total / 100}</Heading>
+        {salesItems?.length > 0 && (
+          <FormInput form={form} placeholder="Paid" label="paid" />
+        )}
+      </FormModal>
     </div>
   );
 }
