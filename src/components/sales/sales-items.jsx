42167e4057e6f2678401122eb2649c39169ceeff
tayef mahmud <mahmudovi183@gmail.com>
2024-10-31 15:49:04 +0600

    suspense loading new sale page

--- src/components/sales/sales-items.jsx
+++ src/components/sales/sales-items.jsx
@@ -1,14 +1,26 @@
 import { SalesProductCard } from "@/components/sales/sales-product-card";
 import { getData } from "@/utils/api-calls";
+import { Suspense } from "react";
+import { Loading } from "../loading";
 
-export async function SalesItems() {
+async function Products() {
   const res = await getData("products", 0);
 
   return (
-    <div className="space-y-4">
+    <>
       {res.response?.payload?.map((product, index) => (
         <SalesProductCard key={index} product={product} />
       ))}
+    </>
+  );
+}
+
+export async function SalesItems() {
+  return (
+    <div className="space-y-4">
+      <Suspense fallback={<Loading className="py-8" />}>
+        <Products />
+      </Suspense>
     </div>
   );
 }
