9ebf557a7a04f63da0fc6e06f10887360b73e02a
tayef mahmud <mahmudovi183@gmail.com>
2024-12-28 12:22:21 +0600

    disabled logo

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -6,12 +6,12 @@ export function Logo() {
   return (
     <Link href="/">
       <figure className="h-[50px] w-[150px]">
-        <Image
+        {/* <Image
           src={logo}
           alt="zeris"
           className="filter invert dark:invert-0"
           sizes="120px"
-        />
+        /> */}
       </figure>
     </Link>
   );
