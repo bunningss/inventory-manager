541975c56e2579b6c9240e421939cce2237bc3ab
tayef mahmud <mahmudovi183@gmail.com>
2025-01-03 23:43:18 +0600

    try converting cookie exp time to number

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 
 export async function setCookie(name, value) {
   cookies().set(name, value, {
-    maxAge: process.env.TOKEN_EXPIRY_TIME,
+    maxAge: Number(process.env.TOKEN_EXPIRY_TIME),
     httpOnly: true,
     sameSite: "strict",
     secure: process.env.NODE_ENV === "production",
